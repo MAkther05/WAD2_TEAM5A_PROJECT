f13241dfadfe52df4214f4694189d7f160624848
2769391c <2769391c@student.gla.ac.uk>
2025-03-04 12:41:28 +0000

    Updated models.py so that some fields can be left blank, changed average_rating to a computed property rather than database field, changed slug to only be set when empty

--- ScreenCritic/models.py
+++ ScreenCritic/models.py
@@ -6,7 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
-    profile_picture = models.ImageField(upload_to='profile_images/', blank=True)
+    profile_picture = models.ImageField(upload_to='profile_images/', blank=True, null=True)
 
     def __str__(self):
         return self.user.username
@@ -16,15 +16,19 @@ class Media(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
     type = models.CharField(max_length=20)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     cover_image = models.ImageField(upload_to='cover_images/', blank=True, null=True)
-    duration = models.IntegerField()
+    duration = models.IntegerField(blank=True, null=True)
     release_date = models.DateTimeField()
-    average_rating = models.IntegerField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        if not self.slug:
+            self.slug = slugify(self.title)
         super(Media, self).save(*args, **kwargs)
+
+    @property
+    def average_rating(self):
+        return Review.objects.filter(media_id=self).aggregate(models.Avg('rating'))['rating__avg'] or 0
 
     def __str__(self):
         return self.title
@@ -33,7 +37,7 @@ class Review(models.Model):
     review_id = models.AutoField(primary_key=True)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     media_id = models.ForeignKey(Media, on_delete=models.CASCADE)
-    review = models.TextField()
+    review = models.TextField(blank=True, null=True)
     rating = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
