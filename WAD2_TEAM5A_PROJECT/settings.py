6055923f01702e99f979d7f1b1983eadc294aa2c
“Mutasim <2841836a@student.gla.ac.uk>
2025-03-09 12:42:12 +0000

    changed one line such that whenever you extend from base.html for any webpage you include {% extends 'ScreenCritic/<name>.html' %} and keeps it very similar to rango

--- WAD2_TEAM5A_PROJECT/settings.py
+++ WAD2_TEAM5A_PROJECT/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'WAD2_TEAM5A_PROJECT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates/ScreenCritic")],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
