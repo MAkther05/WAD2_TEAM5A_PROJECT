adf4aa83ae90c9d7f94f21597a67119f7ad1da8c
Kylee Down <3095263d@student.gla.ac.uk>
2025-03-21 22:41:45 +0000

    movie complete (cannot test when logged in)

--- ScreenCritic/views.py
+++ ScreenCritic/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse 
 from django.db.models import Count, Avg
-from ScreenCritic.models import Media, Review
+from ScreenCritic.models import Media, Review, UserFavouriteGenre
 from django.urls import reverse
 
 # Create your views here.
@@ -11,13 +11,35 @@ def home(request):
 def movie_list(request):
     movies = Media.objects.filter(type='Movie').order_by('-release_date')
 
+    suggested_movies = []
+    if request.user.is_authenticated:
+        favorite_genres = UserFavouriteGenre.objects.filter(user=request.user).values_list('genre', flat=True)
+        if favorite_genres.exists():
+            suggested_movies = Media.objects.filter(genres__in=favorite_genres).distinct()[:10]  # Get unique media
+
+
     trending_movies = (Media.objects.filter(type='Movie').annotate(avg_rating=Avg('review__rating')).order_by('-avg_rating')[:20]) #get recommended media (other media of same type)
-    
+    movies_alphabetically = (Media.objects.filter(type='Movie').order_by('title'))
+
+    movies_by_genre = {}
+
+    for movie in Media.objects.filter(type='Movie').prefetch_related('genres'):
+        for genre in movie.genres.all():
+            if genre.name not in movies_by_genre:
+                movies_by_genre[genre.name] = []
+            movies_by_genre[genre.name].append(movie)
+
+        
+
 
     context = {
         'media_list': movies,
         'media_type': 'Movies',
         'trending_movies': trending_movies,
+        'movies_alphabetically': movies_alphabetically,
+        'movies_by_genre': movies_by_genre,
+        'suggested_movies': suggested_movies,
+        
     }
     return render(request, 'ScreenCritic/media.html', context)
 
