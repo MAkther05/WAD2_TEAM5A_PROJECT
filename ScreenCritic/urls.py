caa698747d71b97785ccde448f9521891ac51bce
Dullmxr <148992623+Dullmxr@users.noreply.github.com>
2025-03-25 09:31:37 +0300

    Update urls.py

--- ScreenCritic/urls.py
+++ ScreenCritic/urls.py
@@ -6,17 +6,18 @@ app_name = 'ScreenCritic'
 urlpatterns = [
     path('', views.home, name='home'),  
 
-    # TV Shows
+   
     path('tv/', views.tv_list, name='tv_list'),
     path('tv/<slug:slug>/', views.media_detail, {'media_type': 'TV Show'}, name='tv_detail'),
 
-    # Movies
+    
     path('movies/', views.movie_list, name='movie_list'),
     path('movies/<slug:slug>/', views.media_detail, {'media_type': 'Movie'}, name='movie_detail'),
 
-    # Games
+    
     path('games/', views.game_list, name='game_list'),
     path('games/<slug:slug>/', views.media_detail, {'media_type': 'Game'}, name='game_detail'),
 
+    
     path('review/<int:review_id>/like/', views.like_review, name='like_review'),
 ]
