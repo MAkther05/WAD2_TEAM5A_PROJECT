f094b51e214c5f6d0fd1c102538b7c5953cd504b
Dullmxr <148992623+Dullmxr@users.noreply.github.com>
2025-03-25 09:29:21 +0300

    Update views.py

--- ScreenCritic/views.py
+++ ScreenCritic/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.db.models import Count, Avg
 from ScreenCritic.models import Media, Review, ReviewLike, UserFavouriteGenre
 
+
 def home(request):
     return render(request, 'ScreenCritic/index.html')
+
+
+def tv_list(request):
+    shows = Media.objects.filter(type='TV Show').order_by('-release_date')
+
+    suggested_shows = []
+    if request.user.is_authenticated:
+        favorite_genres = UserFavouriteGenre.objects.filter(user=request.user).values_list('genre', flat=True)
+        if favorite_genres.exists():
+            suggested_shows = Media.objects.filter(type='TV Show', genres__in=favorite_genres).distinct()[:10]
+
+    trending_shows = Media.objects.filter(type='TV Show').annotate(avg_rating=Avg('review__rating')).order_by('-avg_rating')[:20]
+    shows_alphabetically = Media.objects.filter(type='TV Show').order_by('title')
+
+    context = {
+        'media_list': shows,
+        'media_type': 'TV Shows',
+        'trending_media': trending_shows,
+        'media_alphabetically': shows_alphabetically,
+        'suggested_media': suggested_shows,
+    }
+    return render(request, 'ScreenCritic/media.html', context)
+
 
 def movie_list(request):
     movies = Media.objects.filter(type='Movie').order_by('-release_date')
@@ -13,25 +37,38 @@ def movie_list(request):
     if request.user.is_authenticated:
         favorite_genres = UserFavouriteGenre.objects.filter(user=request.user).values_list('genre', flat=True)
         if favorite_genres.exists():
-            suggested_movies = Media.objects.filter(genres__in=favorite_genres).distinct()[:10]
+            suggested_movies = Media.objects.filter(type='Movie', genres__in=favorite_genres).distinct()[:10]
 
-    trending_movies = (Media.objects.filter(type='Movie').annotate(avg_rating=Avg('review__rating')).order_by('-avg_rating')[:20])
-    movies_alphabetically = (Media.objects.filter(type='Movie').order_by('title'))
-
-    movies_by_genre = {}
-
-    for movie in Media.objects.filter(type='Movie').prefetch_related('genres'):
-        for genre in movie.genres.all():
-            if genre.name not in movies_by_genre:
-                movies_by_genre[genre.name] = []
-            movies_by_genre[genre.name].append(movie)
+    trending_movies = Media.objects.filter(type='Movie').annotate(avg_rating=Avg('review__rating')).order_by('-avg_rating')[:20]
+    movies_alphabetically = Media.objects.filter(type='Movie').order_by('title')
 
     context = {
         'media_list': movies,
         'media_type': 'Movies',
-        'trending_movies': trending_movies,
-        'movies_alphabetically': movies_alphabetically,
-        'movies_by_genre': movies_by_genre,
-        'suggested_movies': suggested_movies,
+        'trending_media': trending_movies,
+        'media_alphabetically': movies_alphabetically,
+        'suggested_media': suggested_movies,
+    }
+    return render(request, 'ScreenCritic/media.html', context)
+
+
+def game_list(request):
+    games = Media.objects.filter(type='Game').order_by('-release_date')
+
+    suggested_games = []
+    if request.user.is_authenticated:
+        favorite_genres = UserFavouriteGenre.objects.filter(user=request.user).values_list('genre', flat=True)
+        if favorite_genres.exists():
+            suggested_games = Media.objects.filter(type='Game', genres__in=favorite_genres).distinct()[:10]
+
+    trending_games = Media.objects.filter(type='Game').annotate(avg_rating=Avg('review__rating')).order_by('-avg_rating')[:20]
+    games_alphabetically = Media.objects.filter(type='Game').order_by('title')
+
+    context = {
+        'media_list': games,
+        'media_type': 'Games',
+        'trending_media': trending_games,
+        'media_alphabetically': games_alphabetically,
+        'suggested_media': suggested_games,
     }
     return render(request, 'ScreenCritic/media.html', context)
