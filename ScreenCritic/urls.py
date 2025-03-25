387620af828aa89afcd3bcf2c4c33ee40c1916e1
Dullmxr <148992623+Dullmxr@users.noreply.github.com>
2025-03-25 09:21:26 +0300

    Update urls.py

--- ScreenCritic/urls.py
+++ ScreenCritic/urls.py
@@ -4,22 +4,19 @@ from ScreenCritic import views
 app_name = 'ScreenCritic'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  
 
     # TV Shows
     path('tv/', views.tv_list, name='tv_list'),
     path('tv/<slug:slug>/', views.media_detail, {'media_type': 'TV Show'}, name='tv_detail'),
-    # path('tv/<slug:slug>/review/', views.media_review, {'media_type': 'TV Show'}, name='tv_review'),
 
     # Movies
     path('movies/', views.movie_list, name='movie_list'),
     path('movies/<slug:slug>/', views.media_detail, {'media_type': 'Movie'}, name='movie_detail'),
-    # path('movies/<slug:slug>/review/', views.media_review, {'media_type': 'Movie'}, name='movie_review'),
 
     # Games
     path('games/', views.game_list, name='game_list'),
     path('games/<slug:slug>/', views.media_detail, {'media_type': 'Game'}, name='game_detail'),
-    # path('games/<slug:slug>/review/', views.media_review, {'media_type': 'Game'}, name='game_review'),
 
     path('review/<int:review_id>/like/', views.like_review, name='like_review'),
 ]
