78f004d10731516f8a23782b08a8fea3c173f2df
alalidana <2960892A@student.gla.ac.uk>
2025-03-26 11:36:51 +0000

    Finish profile view and edit layout

--- ScreenCritic/models.py
+++ ScreenCritic/models.py
@@ -7,6 +7,7 @@ class UserProfile(models.Model):
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
     profile_picture = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.user.username
--- ScreenCritic/views.py
+++ ScreenCritic/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.db.models import Count, Avg
 from ScreenCritic.models import Media, Review, ReviewLike
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile, Review, Media, Genre, UserFavouriteGenre
+from .forms import ProfileEditForm
 
 # Create your views here.
 def home(request):
@@ -72,3 +76,93 @@ def like_review(request, review_id):
 
     like_count = ReviewLike.objects.filter(review=review).count() #get the number of likes for the review
     return JsonResponse({'likes': like_count, 'liked': liked_status}) #return the number of likes and the liked status
+
+@login_required
+def profile_view(request):
+    user_profile = UserProfile.objects.filter(user=request.user).first()
+    favorite_genres = UserFavouriteGenre.objects.filter(user=request.user)
+    active_tab = request.GET.get("tab", "reviewed")
+
+    # Sorting logic
+    sort_option = request.GET.get("sort", "rating-desc")
+    if sort_option == "rating-asc":
+        order_by_field = "rating"
+    elif sort_option == "date-desc":
+        order_by_field = "-date"
+    elif sort_option == "date-asc":
+        order_by_field = "date"
+    else:
+        order_by_field = "-rating"
+
+    context = {
+        "user_profile": user_profile,
+        "favorite_genres": favorite_genres,
+        "active_tab": active_tab,
+        "current_sort": sort_option,
+    }
+
+    if active_tab == "to-review":
+        user_genres = [fav.genre for fav in favorite_genres]
+        to_review_media = (
+            Media.objects.filter(genres__in=user_genres)
+            .distinct()
+            .exclude(review__user=request.user)
+        )
+        context["to_review_media"] = to_review_media
+
+    elif active_tab == "liked":
+        liked_reviews = Review.objects.filter(reviewlike__user=request.user).order_by(order_by_field)
+        liked_review_ids = ReviewLike.objects.filter(user=request.user).values_list('review_id', flat=True)
+        context["liked_reviews"] = liked_reviews
+        context["liked_review_ids"] = list(liked_review_ids)
+
+    else:
+        reviewed_reviews = Review.objects.filter(user=request.user).order_by(order_by_field)
+        context["reviewed_reviews"] = reviewed_reviews
+
+    return render(request, "ScreenCritic/profile.html", context)
+
+@login_required
+def edit_profile(request):
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    genres = Genre.objects.values("genre_id", "name")
+    user_favorite_genres = UserFavouriteGenre.objects.filter(user=request.user)
+
+    if request.method == "POST":
+        form = ProfileEditForm(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            form.save()
+
+            # If user checked the "Remove Profile Picture" box
+            if "delete_picture" in request.POST:
+                if user_profile.profile_picture:
+                    user_profile.profile_picture.delete(save=False)
+                user_profile.profile_picture = None
+                user_profile.save()
+
+            # Handle genres
+            selected_genres = request.POST.getlist("favorite_genres")
+            removed_genres_str = request.POST.get("removed_genres", "")
+            if removed_genres_str:
+                removed_ids = removed_genres_str.split(",")
+                UserFavouriteGenre.objects.filter(
+                    user=request.user,
+                    genre__genre_id__in=removed_ids
+                ).delete()
+
+            for genre_id in selected_genres:
+                genre_obj, _ = Genre.objects.get_or_create(genre_id=genre_id)
+                UserFavouriteGenre.objects.get_or_create(user=request.user, genre=genre_obj)
+
+            # Redirect to the profile page after saving
+            return redirect("ScreenCritic:profile")
+
+    else:
+        form = ProfileEditForm(instance=user_profile)
+
+    return render(request, "ScreenCritic/edit_profile.html", {
+        "form": form,
+        "user_profile": user_profile,
+        "genres": genres,
+        "user_favorite_genres": user_favorite_genres,
+    })
